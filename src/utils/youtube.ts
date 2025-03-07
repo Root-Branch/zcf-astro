f9983f3af8c40c9d2564c02d067c4dd68a055c73
pbson <phambason639@gmail.com>
2025-03-07 13:06:55 +0000

    feat: forum page implementation

---
+++ src/utils/youtube.ts
@@ -0,0 +1,10 @@
+export function getYouTubeVideoId(url: string): string {
+  if (!url) return '';
+  
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const match = url.match(regExp);
+
+  return (match && match[2].length === 11)
+    ? match[2]
+    : '';
+} 
